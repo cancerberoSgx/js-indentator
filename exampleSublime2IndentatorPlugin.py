4b3d647dc38ed2f67b02fa001391b2ac93be6a96
cancerberoSgx <sebastigurin@gmail.com>
2013-08-12 16:29:14 -0300

    commented sublime2 example plugin

--- exampleSublime2IndentatorPlugin.py
+++ exampleSublime2IndentatorPlugin.py
@@ -1,10 +1,14 @@
+#author: sgurin
+#For install this, first fix the nodePath and indentjsFolderPath according to your system
+#then in sublime 2 goto Tools->new Plugin and there type copy and paste this file
+and for using it, select the javascript code you want to indent and goto View->Show Console and there type view.run_command('jsindentator') and enter
+
+
 import sublime, sublime_plugin, subprocess, os; 
 
-
-class ExampleCommand(sublime_plugin.TextCommand):  
+class JsindentatorCommand(sublime_plugin.TextCommand):  
     def run(self, edit):  
-        #self.view.insert(edit, 0, "Hello, World!")  
-        
+        #self.view.insert(edit, 0, "Hello, World!")        
         nodePath = "c:\\Program Files\\nodejs\\node.exe"; 
         indentjsFolderPath = "C:\\Users\\sgurin\\Desktop\\js-indentator\\jsindentator"; 
         indentjsPath = indentjsFolderPath + "\\indent.js"
